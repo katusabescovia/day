366a78b8b40ce1e094406ec9cc1ef7d729d9c003
katusabe scovia <scoviak588@gmail.com>
2025-03-03 16:53:52 +0300

    added and solved conflicts

--- daystar/daystar/wsgi.py
+++ daystar/daystar/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Set the correct path to the settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daystar.settings')
 
 application = get_wsgi_application()
